97a71ed66356ada5f27d937584ba38e3744ba7fa
Ricardo Hernández <ricardoahe12@gmail>
2021-07-21 21:50:56 -0500

    Añadiendo mas funcionalidades de busqueda

--- scripts/busqueda.js
+++ scripts/busqueda.js
@@ -1,30 +1,80 @@
 import crear from "./crearHTML.js";
 
-const inputText = document.getElementById("buscar")
-const inputAuto = document.getElementById("autocompletar")
-const resultado = document.getElementById("resultados")
+const inputText = document.getElementById("buscar");
+const inputAuto = document.getElementById("autocompletar");
+const resultado = document.getElementById("resultados");
+const galeria = document.getElementById("resultados-busqueda");
+const buscar = document.getElementById("boton-busqueda");
+const cancelar = document.getElementById("boton-cancelar");
+const mas = document.getElementById("ver-mas");
+let suma = 0;
+let busqueda = "";
+let buscarArray = [];
 
 inputText.addEventListener("keyup", async (e) => {
+    suma = 0;
+    busqueda = e.target.value;
+
     if (e.keyCode == 13) {
-        const buscarResponse = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=EhKz1YoCvGNKu8jysQQw0rBVAlYgagwK&q=${e.target.value}`)
+        const buscarResponse = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=EhKz1YoCvGNKu8jysQQw0rBVAlYgagwK&q=${busqueda}&limit=40`)
         const buscarJson = await buscarResponse.json();
-        const buscarArray = buscarJson.data;
-        crear.busquedaGifs(e.target.value, buscarArray);
-        resultado.className = "resultados-busqueda"
+        buscarArray = buscarJson.data;
+        crear.busquedaGifs(busqueda, buscarArray, 12 + suma);
+        if (galeria.innerHTML != ""){
+            resultado.className = "resultados-busqueda";
+        }
     }
     
     else if (e.target.value != ""){
-        const autoResponse = await fetch(`https://api.giphy.com/v1/gifs/search/tags?api_key=EhKz1YoCvGNKu8jysQQw0rBVAlYgagwK&q=${e.target.value}`);
+        const autoResponse = await fetch(`https://api.giphy.com/v1/gifs/search/tags?api_key=EhKz1YoCvGNKu8jysQQw0rBVAlYgagwK&q=${busqueda}`);
         const autoJson = await autoResponse.json();
         const autoArray = autoJson.data;
         crear.autocompletar(autoArray);
         e.target.className = "buscando";
+        buscar.className = "buscar-activo";
+        cancelar.className = "cancelar-activo";
         inputAuto.className = "autocompletar";
     }
 
     else {
         e.target.className = "";
         inputAuto.className = "inactivo";
+        buscar.className = "boton-busqueda";
+        cancelar.className = "boton-cancelar";
         crear.autocompletar([])
     }
 })
+
+buscar.addEventListener("click", async (e) => {
+    if (busqueda != ""){
+        const buscarResponse = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=EhKz1YoCvGNKu8jysQQw0rBVAlYgagwK&q=${busqueda}&limit=40`)
+        const buscarJson = await buscarResponse.json();
+        buscarArray = buscarJson.data;
+        crear.busquedaGifs(busqueda, buscarArray, 12 + suma);
+        resultado.className = "resultados-busqueda";
+    }
+})
+
+cancelar.addEventListener("click", (e) => {
+    inputText.value = "";
+    busqueda = "";
+    inputText.className = "";
+    inputAuto.className = "inactivo";
+    buscar.className = "boton-busqueda";
+    cancelar.className = "boton-cancelar";
+    crear.autocompletar([])
+})
+
+mas.addEventListener("click", async (e) => {
+    busqueda = `${inputText.value}`;
+    suma += 8;
+    if ((12 + suma) < buscarArray.length){
+        crear.busquedaGifs(busqueda, buscarArray, 12 + suma);
+    }
+})
+
+/*Esto va en crearHTML en la funcion que crea los trendinng tags, aqui por alguna razón 
+no esta tomando los anchors que necesitamos para obtener los tags en trending,
+lo moviste de sitio haciendo pruebas para ver si arreglabas el botón de ver mas cuando hacias
+la busqueda por los tags en trending*/
+
--- scripts/crearHTML.js
+++ scripts/crearHTML.js
@@ -1,9 +1,3 @@
-function crearTrendings(stringArray){
-    const trendingTags = document.getElementById("trending-tags");
-    trendingTags.innerHTML = `<a href="#">${stringArray[0]}</a>, <a href="#">${stringArray[1]}</a>, <a href="#">${stringArray[2]}</a>, <a href="#">${stringArray[3]}</a>, <a href="#">${stringArray[4]}</a>
-    `;
-}
-
 function crearTrendingGif(gifArray){
     if (gifArray != []) {
         const trendingGif = document.getElementById("galeria-gif");
@@ -37,33 +31,71 @@ function autocompletar(autoArray){
         for (const sugerencia of autoArray){
             auto.innerHTML +=
                 `
-                <li>${sugerencia.name}</li>
+                <div class="sugerencia">
+                    <i class="fas fa-search"></i>
+                    <li>${sugerencia.name}</li>
+                </div>
                 `;
         }
     }
 }
 
-function busquedaGifs(busqueda, gifArray){
+function busquedaGifs(busqueda, gifArray, clave){
+    const seccion = document.getElementById("resultados")
     const titulo = document.getElementById("titulo-busqueda");
+    const noResult = document.getElementById("lo-sentimos")
     const resultados = document.getElementById("resultados-busqueda");
-    titulo.innerHTML = `${busqueda}`;
-    resultados.innerHTML = "";
-    for (let counter = 0; counter < 16; counter++){
-        resultados.innerHTML += `
-        <div class="tarjeta gif-busqueda">
-            <img src="${gifArray[counter].images.fixed_height.url}" class="gif" alt="gif">
-            <div class="fondo-tarjeta">
-                <div class="contenedor-botones">
-                    <div class="boton-tarjeta" id="favoritos"><i class="far fa-heart"></i></div>
-                    <div class="boton-tarjeta" id="descargar"><i class="fas fa-download"></i></div>
-                    <div class="boton-tarjeta" id="expandir"><i class="fas fa-expand-alt"></i></div>
+    const mas = document.getElementById("ver-mas");
+    
+    if (gifArray.length != 0){
+        titulo.innerHTML = `${busqueda}`;
+        resultados.innerHTML = "";
+        for (let counter = 0; counter < clave; counter++){
+            resultados.innerHTML += `
+            <div class="tarjeta gif-busqueda">
+                <img src="${gifArray[counter].images.fixed_height.url}" class="gif" alt="gif">
+                <div class="fondo-tarjeta">
+                    <div class="contenedor-botones">
+                        <div class="boton-tarjeta" id="favoritos"><i class="far fa-heart"></i></div>
+                        <div class="boton-tarjeta" id="descargar"><i class="fas fa-download"></i></div>
+                        <div class="boton-tarjeta" id="expandir"><i class="fas fa-expand-alt"></i></div>
+                    </div>
+                
+                    <h4 class="info-tarjeta">${gifArray[counter].username}</h4>
+                    <h3 class="info-tarjeta">${gifArray[counter].title}</h3>
                 </div>
-            
-                <h4 class="info-tarjeta">${gifArray[counter].username}</h4>
-                <h3 class="info-tarjeta">${gifArray[counter].title}</h3>
             </div>
-        </div>
-        ` 
+            ` 
+        }
+    }
+    else {
+        titulo.innerHTML = "Lorem Ipsum";
+        seccion.className = "sin-resultados";
+        noResult.className = "contenedor-noresult";
+        resultados.className = "inactivo";
+        mas.className = "inactivo";
+    }
+}
+
+function crearTrendings(stringArray){
+    const trendingTags = document.getElementById("trending-tags");
+    trendingTags.innerHTML = `<a class="tag" href="#trending-tags">${stringArray[0]}</a>, <a class="tag" href="#trending-tags">${stringArray[1]}</a>, <a class="tag" href="#trending-tags">${stringArray[2]}</a>, <a class="tag" href="#trending-tags">${stringArray[3]}</a>, <a class="tag" href="#trending-tags">${stringArray[4]}</a>
+    `;
+
+    /*Falta arreglar que no funciona el botón de ver mas*/
+
+    const buscar = document.getElementById("buscar");
+    const resultado = document.getElementById("resultados")
+    const tags = document.querySelectorAll(".tag")
+    for (let tag of tags){
+        tag.addEventListener("click", async (e) => {
+            buscar.value = `${e.target.innerHTML}`;
+            const buscarResponse = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=EhKz1YoCvGNKu8jysQQw0rBVAlYgagwK&q=${e.target.innerHTML}`)
+            const buscarJson = await buscarResponse.json();
+            const buscarArray = buscarJson.data;
+            busquedaGifs(e.target.innerHTML, buscarArray, 12);
+            resultado.className = "resultados-busqueda";
+        })
     }
 }
 
